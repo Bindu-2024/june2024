6540585d9dfea041c50713c51fdb445fc3c4f4d4
Bindu Supraja <bindus@infinite.com>
2024-06-18 17:15:39 +0530

    check_sign

---
+++ check_equality/check_equality/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace check_sign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num > 0)
+            {
+                Console.WriteLine("the number is positive.");
+                Console.ReadLine();
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("the number is negative.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("the number is zero.");
+                Console.ReadLine();
+            }
+            ;
+        }
+    }
+}

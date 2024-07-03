2d4a6f8740a129fb3a46edcaa8101a22232e2f9f
Bindu Supraja <bindus@infinite.com>
2024-07-03 12:54:41 +0530

    cc3

---
+++ csharp/assesments/cc3/cc3/Box.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc3
+{
+    public class Box
+    {
+        public int Length { get; set; }
+        public int Breadth { get; set; }
+        public Box(int length = 0, int breadth = 0)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+        public static Box AddBoxes(Box b1, Box b2)
+        {
+            return new Box(b1.Length + b2.Length, b1.Breadth + b2.Breadth);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: " + Length + " , Breadth:" + Breadth);
+            Console.Read();
+        }
+    }
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            Box box1 = new Box(5, 10);
+            Box box2 = new Box(3, 6);
+            Box box3 = Box.AddBoxes(box1, box2);
+
+            Console.WriteLine("Box 1 details:");
+            box1.Display();
+
+            Console.WriteLine("Box 2 details:");
+            box2.Display();
+
+            Console.WriteLine("Box 3 (Result of addition) details:");
+            box3.Display();
+        }
+    }
+}
+
---
+++ csharp/assesments/cc3/cc3/cricket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace cc3
+{
+    public class cricket
+    {
+        public void pointscalculation(int no_of_matches)
+        {
+            List<int> scores = new List<int>();
+            for(int i = 0; i < no_of_matches; i++)
+            {
+                Console.WriteLine("Enter score for match " + i + 1 );
+                int score = int.Parse(Console.ReadLine());
+                scores.Add(score);
+            }
+            int total_score = 0;
+            foreach(int score in scores)
+            {
+                total_score += score;
+            }
+            double average_score = no_of_matches > 0 ? (double)total_score / no_of_matches : 0;
+            Console.WriteLine("sum of scores: "+total_score);
+            Console.WriteLine("Average score: " + average_score);
+            Console.Read();
+        }
+        public static void Main(string[] args)
+        {
+            cricket crket = new cricket();
+            Console.Write("Enter the number of matches:");
+            int num_matches = int.Parse(Console.ReadLine());
+            crket.pointscalculation(num_matches);
+        }
+    }
+}

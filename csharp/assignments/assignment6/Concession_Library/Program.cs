bcbdc8e62105f690164a250840244dfcc0a9044e
Bindu Supraja <bindus@infinite.com>
2024-07-07 23:21:54 +0530

    assignment6

---
+++ csharp/assignments/assignment6/Concession_Library/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession_Library
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            ConcessionLibrary.ConcessionCalculator calculator = new ConcessionLibrary.ConcessionCalculator();
+            calculator.CalculateConcession(name, age);
+
+            Console.ReadLine();
+        }
+    }
+}

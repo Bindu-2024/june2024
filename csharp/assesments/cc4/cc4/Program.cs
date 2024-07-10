99b1db4d6f75e75db93a4e8af2b58155344aac7f
Bindu Supraja <bindus@infinite.com>
2024-07-10 11:11:18 +0530

    cc4

---
+++ csharp/assesments/cc4/cc4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+namespace cc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "existingfile.txt";
+            string textToAppend = "This is BinduSupraja appended text.";
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(textToAppend);
+            }
+            Console.WriteLine("Text appended successfully.");
+            Console.Read();
+        }
+    }
+}
---
+++ csharp/assesments/cc4/cc4/calculations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc4
+{
+    class calculations
+    {
+        public delegate int Calculator(int x, int y);
+        static void Main()
+        {
+            Calculator add = new Calculator(Add);
+            Calculator subtract = new Calculator(Subtract);
+            Calculator multiply = new Calculator(Multiply);
+
+            Console.Write("enter first number:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter second number:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Addition: {add(n1, n2)}");
+            Console.WriteLine($"Subtraction: {subtract(n1, n2)}");
+            Console.WriteLine($"Multiplication: {multiply(n1, n2)}");
+            Console.Read();
+
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
